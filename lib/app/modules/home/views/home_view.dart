0c5ae8db5894d75d87e420c19b156c5f2b9e1314
polinakhorkova <polinakhorkova@mail.ru>
2024-03-05 14:53:10 +0500

    cringe

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,14 +10,36 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        toolbarHeight: 100, 
+        title:   RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'SNEAKER\n',
+                  style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900, fontSize: 40),
+                ),
+                TextSpan(
+                  text: 'MARKET',
+                  style: TextStyle(color: Colors.black,fontWeight: FontWeight.w900, fontSize: 40),
+                ),
+              ],
+            ),
+          ),
+
+
+       
+        backgroundColor: Color.fromRGBO(154, 23, 80, 1),
       ),
-      body: const Center(
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
         child: Text(
           'HomeView is working',
           style: TextStyle(fontSize: 20),
+          
         ),
+        decoration: BoxDecoration(color: Color.fromRGBO(227, 226, 223, 1),),),
+        
       ),
     );
   }
